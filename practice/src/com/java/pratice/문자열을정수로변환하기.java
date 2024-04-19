40faf2df17b1367564cd893d270ada7c51f71f97
하이미디어학원 <@DESKTOP-QN0GJR9>
2024-04-19 12:07:54 +0900

    2024/04/19

---
+++ practice/src/com/java/pratice/문자열을정수로변환하기.java
@@ -0,0 +1,14 @@
+package com.java.pratice;
+
+public class 문자열을정수로변환하기 {
+
+	public static void main(String[] args) {
+
+		String n_str= "10";
+		
+		Integer answer = Integer.valueOf(n_str);
+		System.out.println(answer);
+		
+	}
+
+}
---
+++ practice/src/com/java/pratice/배열의유사도.java
@@ -0,0 +1,18 @@
+package com.java.pratice;
+
+public class 배열의유사도 {
+
+	public static void main(String[] args) {
+
+		String s1[] = {"a","b","c"};
+		String s2[] = {"com", "b", "d", "p", "c"};
+		int result =0;
+		
+		for(int i=1; i<s1.length; i++)
+			for(int j=1; j<s2.length; j++) {
+				
+			}
+		
+	}
+
+}
