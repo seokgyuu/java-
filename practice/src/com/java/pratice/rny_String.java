99a5a1b7c9d70bb858d58025c7d55b2c01e03222
하이미디어학원 <@DESKTOP-QN0GJR9>
2024-04-22 10:37:46 +0900

    2024/04/22

---
+++ practice/src/com/java/pratice/rny_String.java
@@ -0,0 +1,11 @@
+package com.java.pratice;
+
+public class rny_String {
+
+	public static void main(String[] args) {
+
+		String rny_String = "masterpiece";
+		
+	}
+
+}
---
+++ practice/src/com/java/pratice/문자열의앞의n글자.java
@@ -0,0 +1,14 @@
+package com.java.pratice;
+
+public class 문자열의앞의n글자 {
+
+	public static void main(String[] args) {
+
+		String my_String = "ProgrammerS123";
+		int n =11;
+	
+		System.out.println(my_String.substring(0,n));
+		
+	}
+
+}
